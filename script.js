44fe8a7b2828be08ee2707201c6184d3d56e5e5d
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-11 13:32:04 +0500

    add the array methods and arrar data

---
+++ script.js
@@ -0,0 +1,9 @@
+var command = prompt("Enter your array method:");
+
+var arr = ["Lorem ipsumabo doloribus distinctio perspiciatis a harum, illum enim, id ea! Delectus."];
+
+if (command == length()) {
+    arr.length();
+}  else if(command == toString()) {
+    arr.toString();
+}
