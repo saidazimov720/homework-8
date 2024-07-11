eec9c001164cfb6ac5107963b866a6bab1236e57
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-11 14:22:09 +0500

    corrections: add the split method to the splice arguments prompt

--- script.js
+++ script.js
@@ -3,21 +3,18 @@ var command = prompt("Enter your array method:");
 var arr = ["Lorem ipsumabo doloribus distinctio perspiciatis a harum, illum enim, id ea! Delectus."];
 
 if (command == "length") {
-    arr.length();
-}  else if(command == "toString") {
-    arr.toString();
+    alert(arr.length); 
+} else if (command == "toString") {
+    alert(arr.toString()); 
+} else if (command == "shift") {
+    arr.shift(); 
     alert(arr);
-}
- else if(command == "shift") {
-    arr.shift();
+} else if (command == "unshift") {
+    var value = prompt("Enter a value to add to the beginning of the array:");
+    arr.unshift(value); 
     alert(arr);
-}
- else if(command == "unshift") {
-    arr.unshift();
-    alert(arr);
-}
-else if (command == "splice") {
-    var arguments = prompt("Enter the arguments");
+} else if (command == "splice") {
+    var arguments = prompt("Enter the argumnets with comma:").split(',');
     var startsp = parseInt(arguments[0]);
     var delsp = parseInt(arguments[1]);
     arr.splice(startsp, delsp); 
@@ -26,17 +23,12 @@ else if (command == "splice") {
     var newValue = prompt("Enter a value to concatenate to the array:");
     var newArr = arr.concat([newValue]); 
     alert(newArr);
-} else if(command == "slice"){
-    var arguments2 = prompt("Enter the arguments");
+} else if (command == "slice") {
+    var arguments2 = prompt("Enter the start and end indices for slice, separated by a comma:").split(',');
     var startsl = parseInt(arguments2[0]);
     var endsl = parseInt(arguments2[1]);
-    arr.slice(startsl, endsl);
-
-    if (arguments2 < 2) {
-        arr.slice(startsl);
-    }
-    alert(arr);
-} 
-else{
+    var slicedArr = arr.slice(startsl, endsl); 
+    alert(slicedArr);
+} else {
     alert("Invalid method");
 }
