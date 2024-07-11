26040a93a26e48e3938ff5df3d335be4a6cf49b5
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-11 13:56:54 +0500

    start corrections

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var command = prompt("Enter your array method:");
 
 var arr = ["Lorem ipsumabo doloribus distinctio perspiciatis a harum, illum enim, id ea! Delectus."];
 
-if (command == length()) {
+if (command == "length") {
     arr.length();
 }  else if(command == toString()) {
     arr.toString();
